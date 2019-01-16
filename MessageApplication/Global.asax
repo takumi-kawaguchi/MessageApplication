4b14c162e9e29133995362f62bb2cfaf9d728bb8
Takumi Kawaguchi <tcgds4@gmail.com>
2019-01-16 22:59:21 +0900

    プロジェクト ファイルを追加します。

---
+++ MessageApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MessageApplication.MvcApplication" Language="C#" %>
---
+++ MessageApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MessageApplication.Startup))]
+namespace MessageApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
